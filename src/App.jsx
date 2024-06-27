35c2009273f1af34f316280f9d5771d2421cda69
Anwar-Abdel <anwarabdeljaber0@gmail.com>
2024-06-26 22:55:56 -0500

    feat: imported css into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import './index.css'
+import './App.css'
 const App = () => {
   const [team, setTeam] = useState([]);
   const [money, setMoney] = useState(100);
